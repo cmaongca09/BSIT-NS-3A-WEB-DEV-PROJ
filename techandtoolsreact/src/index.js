f21ce76a1d803b8c53ac7c28d1e5098eb172b3bb
Alegre,Jonathan <115022857+Alegre-Jonathan@users.noreply.github.com>
2024-01-24 03:45:07 +0800

    Setting up the API
    Play Testing The API

--- techandtoolsreact/src/index.js
+++ techandtoolsreact/src/index.js
@@ -8,3 +8,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const url = 'https://techandtools.p.rapidapi.com/%7BPATH%7D';
+const options = {method: 'GET'};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
