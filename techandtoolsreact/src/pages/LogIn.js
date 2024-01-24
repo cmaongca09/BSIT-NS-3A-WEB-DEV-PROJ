19af5c334252b9065610f10523ab10ec40ab6de6
archieagulay <131853891+archieagulay@users.noreply.github.com>
2024-01-24 18:07:08 +0800

    Updated LogIn.js and SignUp.js
    I created a login form and a signup form with hide key or password feature

--- techandtoolsreact/src/pages/LogIn.js
+++ techandtoolsreact/src/pages/LogIn.js
@@ -1,8 +1,64 @@
+import React, {useState} from "react";
+
 
 export const LogIn = () => {
-  return <h1> This is the LogIn page</h1>;
+    const bodyStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        margin: '0',
+    };
+
+    const formStyle = {
+        width: '300px',
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '10px',
+        backgroundColor: 'lightgray',
+        textAlign: 'left',
+    };
+
+    const inputStyle = {
+        width: '100%',
+    };
+    const titleStyle ={
+        fontSize: '25px',
+        fontWeight: 'bold',
+    };
+
+ const [showPassword, setShowPassword] = useState (false);
+  function redirectSignup () {
+    window.location.href= "/SignUp";
+}
+  return (
+    <section>
+      <div style={bodyStyle}>
+      <form style={formStyle}>
+                <label style={titleStyle}> Login Form </label>
+                    <hr/>
+                <label> TUP Student ID No. </label>
+                    <br/>
+                <input type="textbox" required id="studentCode" style={inputStyle}/>
+                    <br/>
+
+                <label> Password </label>
+                    <br/>
+                <input type={showPassword ? 'text' : 'password'} required id="password" style={inputStyle}/>
+                <button type='button' onClick={() => {
+                        return setShowPassword(!showPassword);
+                    }}>
+                        {showPassword ? 'Hide Password' : 'Show Password'}
+                    </button>
+                    <br/>
+                    <br/>
+                <button type="submit">Log-In</button>
+                    <br/>
+                    <br/>
+                <button type="button" onClick={redirectSignup}> Sign-up </button>
+
+            </form>
+      </div>
+    </section>
+  );
 };
-
-
-
-
--- techandtoolsreact/src/pages/SignUp.js
+++ techandtoolsreact/src/pages/SignUp.js
@@ -1,3 +1,91 @@
+import React, {useState} from 'react';
+
+function passwordCheck (){
+    var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("confirmPassword");
+
+    if (password !== confirmPassword) {
+        confirmPassword.setCustomValidity("Passwords don't match")
+    }
+    else {
+        confirmPassword.setCustomValidity("")
+    }
+}
+    
+
 export const SignUp = () => {
-  return <h1> This is the SignUp page</h1>;
-};
+
+    const bodyStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '60vh',
+        margin: '5',
+    };
+
+    const formStyle = {
+        width: '300px',
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '10px',
+        backgroundColor: 'lightgray',
+        textAlign: 'left',
+    };
+
+    const inputStyle = {
+        width: '100%',
+    };
+    const title ={
+        fontSize: '25px',
+        fontWeight: 'bold',
+    };
+    const [showPassword, setShowPassword] = useState (false);
+    return (
+    <section>
+        <div style={bodyStyle}>
+            <form style={formStyle}> 
+                <label id='title' style={title}> Sign-Up Form</label>
+                <hr/>
+
+                <label> First Name </label>
+                <br/>
+                <input type='text' required style={inputStyle}/>
+                <br/>
+
+                <label> Middle Initial </label>
+                <br/>
+                <input type='text' maxLength={2} required style={inputStyle}/>
+                <br/>
+
+                <label> Last Name </label>
+                <br/>
+                <input type='text' required style={inputStyle}/>
+                <br/>
+
+                <label> TUP Student ID No. </label>
+                <br/>
+                <input type='text' required style={inputStyle}/>
+                <br/>
+
+                <label> Password</label>
+                <br/>
+                <input type={showPassword ? 'text' : 'password'} required id='password' style={inputStyle}/>
+                <br/>
+
+                <label> Confirm Password</label>
+                <br/>
+                <input type={showPassword ? 'text' : 'password'} required id='confirmPassword' style={inputStyle}/>
+
+                <button type='button' onClick={() => {
+                        return setShowPassword(!showPassword);
+                    }}>
+                        {showPassword ? 'Hide Password' : 'Show Password'}
+                    </button>
+                <br/>
+                <br/>
+                <button type='submit' onClick={passwordCheck}> Sign-Up </button> 
+            </form>
+        </div>
+        </section>
+    );
+    }
