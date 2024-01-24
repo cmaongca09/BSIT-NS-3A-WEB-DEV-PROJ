b0a6775c03832b9a85e92d659249e339bb905e86
cmaongca09 <clarencedave.maongca@tup.edu.ph>
2024-01-24 20:49:49 +0800

    Update App.js, SampleProducts.json, , index.js, Profile..js, Shop.js
    Re editing Links in App
    Adding dataset on SampleProducts.json
    removing API index.js
    removing Profile.js content temp
    changing overhaul Shop.js adding few modals

--- techandtoolsreact/src/pages/Shop.js
+++ techandtoolsreact/src/pages/Shop.js
@@ -1,160 +1,36 @@
-import React from "react";
-import Blthmod from "../imgassets/Bluetooth-Module_HC-05.png";
-import Capacitor from "../imgassets/Capacitor-10uf-Electrolytic.png";
-import RasPi from "../imgassets/Development-Board_Raspberry-Pi-4.png";
-import Diodes from "../imgassets/Diodes_1N4148.png";
-import EthSh from "../imgassets/Ethernet-Shield_W5100.png";
-import GPSMod from "../imgassets/GPS-Module_Neo-6M.png";
-import HumSen from "../imgassets/Humidity-Sensor_HIH-6130.png";
-import JumpWires from "../imgassets/Jumper-Wires_Male-To-Male-Jumper-Wires.png";
-import LCD from "../imgassets/LCD-Display_16x2-LCD-Module.png";
-import LED from "../imgassets/LEDS_RGB-LED.png";
-import LogAn from "../imgassets/Logic-Analyzer_Saleae-Logic-Pro-8.png";
-import Arduino from "../imgassets/Microcontroller_Arduino-Uno.png";
-import PIRSen from "../imgassets/Motion-Sensor_PIR-Motion-Sensor.png";
-import MotorMod from "../imgassets/Motor-Driver_L298N-Motor-Driver-Module.png";
-import DigiMulti from "../imgassets/Multimeter_Fluke-117-Digital-Multimeter.png";
-import PowerBank from "../imgassets/Power-Bank_Anker-Powercore-10000.png";
-import PowerSupply from "../imgassets/Power-Supply-Unit_DC-Bench-Power-Supply.png";
-import Resistor from "../imgassets/Resistor-220-Ohm.png";
-import RFIDRead from "../imgassets/RFID-Reader_MFRC522.png";
-import RFIDTags from "../imgassets/RFID-Tags_Mifare-Classic-1K.png";
-import Solder from "../imgassets/Soldering_Station-Weller-WLC100.png";
-import TempSen from "../imgassets/Temperature-Sensor_DHT22.png";
-import USBop from "../imgassets/USB-Oscilloscope_Picoscope-2204A.png";
-import Webcam from "../imgassets/Webcam_Logitech-C920-HD-Pro.png";
-import WiFi from "../imgassets/WiFi-Module_ESP8266.png";
+import { React } from "react";
+import SampleProducts from "../Data/SampleProducts.json";
 export const Shop = () => {
-  console.log(
-    Blthmod,
-    Capacitor,
-    RasPi,
-    Diodes,
-    EthSh,
-    GPSMod,
-    HumSen,
-    JumpWires,
-    LCD,
-    LED,
-    LogAn,
-    Arduino,
-    PIRSen,
-    MotorMod,
-    DigiMulti,
-    PowerBank,
-    PowerSupply,
-    Resistor,
-    RFIDRead,
-    RFIDTags,
-    Solder,
-    TempSen,
-    USBop,
-    Webcam,
-    WiFi
-  );
+  // const [isModalOpen, setModalOpen] = useState(false);
+  // const [selectedProduct, setSelectedProduct] = useState(null);
+
+  // const openModal = (product) => {
+  //   setSelectedProduct(product);
+  //   setModalOpen(true);
+  // };
+
+  // const closeModal = () => {
+  //   setModalOpen(false);
+  // };
+
   return (
     <section>
-      <h1> Shop page</h1>
-      <div>
-        <img src={Blthmod} alt="Bluetooth-Module_HC-05" />
-        <p>Bluetooth-Module_HC-05.png</p>
-      </div>
-      <div>
-        <img src={Capacitor} alt="Capacitor-10uf-Electrolytic" />
-        <p>Capacitor-10uf-Electrolytic</p>
-      </div>
-      <div>
-        <img src={RasPi} alt="Development-Board_Raspberry-Pi-4" />
-        <p>Development-Board_Raspberry-Pi-4</p>
-      </div>
-      <div>
-        <img src={Diodes} alt="Diodes_1N4148" />
-        <p>Diodes_1N4148</p>
-      </div>
-      <div>
-        <img src={EthSh} alt="Ethernet-Shield_W5100" />
-        <p>Ethernet-Shield_W5100</p>
-      </div>
-      <div>
-        <img src={GPSMod} alt="GPS-Module_Neo-6M" />
-        <p>GPS-Module_Neo-6M</p>
-      </div>
-      <div>
-        <img src={HumSen} alt="Humidity-Sensor_HIH-6130" />
-        <p>Humidity-Sensor_HIH-6130</p>
-      </div>
-      <div>
-        <img src={JumpWires} alt="Jumper-Wires_Male-To-Male-Jumper-Wires" />
-        <p>Jumper-Wires_Male-To-Male-Jumper-Wires</p>
-      </div>
-      <div>
-        <img src={LCD} alt="LCD-Display_16x2-LCD-Module" />
-        <p>LCD-Display_16x2-LCD-Module</p>
-      </div>
-      <div>
-        <img src={LED} alt="LEDS_RGB-LED" />
-        <p>LEDS_RGB-LED</p>
-      </div>
-      <div>
-        <img src={LogAn} alt="Logic-Analyzer_Saleae-Logic-Pro-8" />
-        <p>Logic-Analyzer_Saleae-Logic-Pro-8</p>
-      </div>
-      <div>
-        <img src={Arduino} alt="Microcontroller_Arduino-Uno" />
-        <p>Microcontroller_Arduino-Uno</p>
-      </div>
-      <div>
-        <img src={PIRSen} alt="Motion-Sensor_PIR-Motion-Sensor" />
-        <p>Motion-Sensor_PIR-Motion-Sensor</p>
-      </div>
-      <div>
-        <img src={MotorMod} alt="Motor-Driver_L298N-Motor-Driver-Module" />
-        <p>Motor-Driver_L298N-Motor-Driver-Module</p>
-      </div>
-      <div>
-        <img src={DigiMulti} alt="Multimeter_Fluke-117-Digital-Multimeter" />
-        <p>Multimeter_Fluke-117-Digital-Multimeter</p>
-      </div>
-      <div>
-        <img src={PowerBank} alt="Power-Bank_Anker-Powercore-10000" />
-        <p>Power-Bank_Anker-Powercore-10000</p>
-      </div>
-      <div>
-        <img src={PowerSupply} alt="Power-Supply-Unit_DC-Bench" />
-        <p>Power-Supply-Unit_DC-Bench</p>
-      </div>
-      <div>
-        <img src={Resistor} alt="Resistor-220-Ohm" />
-        <p>Resistor-220-Ohm</p>
-      </div>
-      <div>
-        <img src={RFIDRead} alt="RFID-Reader_MFRC522" />
-        <p>RFID-Reader_MFRC522</p>
-      </div>
-      <div>
-        <img src={RFIDTags} alt="RFID-Tags_Mifare-Classic-1K" />
-        <p>RFID-Tags_Mifare-Classic-1K</p>
-      </div>
-      <div>
-        <img src={Solder} alt="Soldering_Station-Weller-WLC100" />
-        <p>RFID-Tags_Mifare-Classic-1K</p>
-      </div>
-      <div>
-        <img src={TempSen} alt="Temperature-Sensor_DHT22" />
-        <p>Temperature-Sensor_DHT22</p>
-      </div>
-      <div>
-        <img src={USBop} alt="USB-Oscilloscope_Picoscope-2204A" />
-        <p>USB-Oscilloscope_Picoscope-2204A</p>
-      </div>
-      <div>
-        <img src={Webcam} alt="Webcam_Logitech-C920-HD-Pro" />
-        <p>Webcam_Logitech-C920-HD-Pro</p>
-      </div>
-      <div>
-        <img src={WiFi} alt="WiFi-Module_ESP8266" />
-        <p>WiFi-Module_ESP8266</p>
-      </div>
+      <h1>Shop page</h1>
+
+      {SampleProducts.map((product, index) => (
+        <button onClick={console.log(product.name)}>
+          <div key={index}>
+            <p>{product.id}</p>
+
+            <img
+              src={require(`../imgassets/${product.imageurl}`)}
+              alt={product.name}
+            />
+
+            <p>{product.name}</p>
+          </div>
+        </button>
+      ))}
     </section>
   );
 };
