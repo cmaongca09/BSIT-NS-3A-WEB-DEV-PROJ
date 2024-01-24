8f41c42d0ca9c1e0a9ce8a66d4cd829535083715
cmaongca09 <clarencedave.maongca@tup.edu.ph>
2024-01-24 23:33:36 +0800

    Added ProductModal.js, SampleProducts.json, Profile.js, Shop.js
    Added Product modal for buyinng
    editing and adding description and quantity
    Adding codes for our profile
    Editing Shop.js

--- techandtoolsreact/src/pages/Shop.js
+++ techandtoolsreact/src/pages/Shop.js
@@ -1,36 +1,40 @@
-import { React } from "react";
+import React, { useState } from "react";
 import SampleProducts from "../Data/SampleProducts.json";
+import ProductModal from "../Component/ProductModal.js";
 export const Shop = () => {
-  // const [isModalOpen, setModalOpen] = useState(false);
-  // const [selectedProduct, setSelectedProduct] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // const openModal = (product) => {
-  //   setSelectedProduct(product);
-  //   setModalOpen(true);
-  // };
+  const openModal = (product) => {
+    setSelectedProduct(product);
+    setIsModalOpen(true);
+  };
 
-  // const closeModal = () => {
-  //   setModalOpen(false);
-  // };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <section>
       <h1>Shop page</h1>
-
       {SampleProducts.map((product, index) => (
-        <button onClick={console.log(product.name)}>
-          <div key={index}>
+        <div key={index}>
+          <button onClick={() => openModal(product)}>
             <p>{product.id}</p>
-
             <img
               src={require(`../imgassets/${product.imageurl}`)}
               alt={product.name}
             />
-
             <p>{product.name}</p>
-          </div>
-        </button>
+          </button>
+        </div>
       ))}
+
+      <ProductModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        product={selectedProduct}
+      />
     </section>
   );
 };
